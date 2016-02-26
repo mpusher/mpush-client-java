e5a8b1a357842aed38041d67d3fb52d573c2f43d
闫逍旭 <yanxx@shinemo.com>
2016-02-26 16:19:46 +0000

    心跳检测修改

--- src/main/java/com/shinemo/mpush/client/DefaultClientListener.java
+++ src/main/java/com/shinemo/mpush/client/DefaultClientListener.java
@@ -48,6 +48,8 @@ import java.util.concurrent.Executor;
                     listener.onHandshakeOk(client, heartbeat);
                 }
             });
+        } else {
+            //do heathCheck
         }
         client.bindUser(ClientConfig.I.getUserId());
     }
--- src/test/java/com/shinemo/mpush/client/MPushClientTest.java
+++ src/test/java/com/shinemo/mpush/client/MPushClientTest.java
@@ -1,6 +1,7 @@
 package com.shinemo.mpush.client;
 
 import com.shinemo.mpush.api.Client;
+import com.shinemo.mpush.api.ClientListener;
 import com.shinemo.mpush.util.DefaultLogger;
 import org.junit.Test;
 
@@ -12,11 +13,10 @@ import static org.junit.Assert.*;
  * Created by ohun on 2016/1/25.
  */
 public class MPushClientTest {
-    private String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCghPCWCobG8nTD24juwSVataW7iViRxcTkey/B792VZEhuHjQvA3cAJgx2Lv8GnX8NIoShZtoCg3Cx6ecs+VEPD2fBcg2L4JK7xldGpOJ3ONEAyVsLOttXZtNXvyDZRijiErQALMTorcgi79M5uVX9/jMv2Ggb2XAeZhlLD28fHwIDAQAB";
-    private String allocServer = "http://allot.mangguoyisheng.com/";
+    private static final String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCghPCWCobG8nTD24juwSVataW7iViRxcTkey/B792VZEhuHjQvA3cAJgx2Lv8GnX8NIoShZtoCg3Cx6ecs+VEPD2fBcg2L4JK7xldGpOJ3ONEAyVsLOttXZtNXvyDZRijiErQALMTorcgi79M5uVX9/jMv2Ggb2XAeZhlLD28fHwIDAQAB";
+    private static final String allocServer = "http://allot.mangguoyisheng.com/";
 
-    @Test
-    public void testStart() throws Exception {
+    public static void main(String[] args) throws Exception {
         Client client = ClientConfig
                 .build()
                 .setPublicKey(publicKey)
@@ -26,12 +26,61 @@ public class MPushClientTest {
                 .setOsVersion("6.0")
                 .setClientVersion("2.0")
                 .setUserId("doctor43test")
-                .setSessionStorageDir(getClass().getResource("/").getFile())
+                .setSessionStorageDir(MPushClientTest.class.getResource("/").getFile())
                 .setLogger(new DefaultLogger())
                 .setLogEnabled(true)
                 .setEnableHttpProxy(false)
+                .setClientListener(new L())
                 .create();
         client.start();
+
         LockSupport.park();
+    }
+
+
+
+
+    public static class L implements ClientListener {
+        Thread thread;
+        boolean flag = true;
+
+        @Override
+        public void onConnected(Client client) {
+            flag = true;
+        }
+
+        @Override
+        public void onDisConnected(Client client) {
+            flag = false;
+        }
+
+        @Override
+        public void onHandshakeOk(final Client client, final int heartbeat) {
+            thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (flag && client.isRunning()) {
+                        try {
+                            Thread.sleep(heartbeat);
+                        } catch (InterruptedException e) {
+                            break;
+                        }
+                        client.healthCheck();
+                    }
+                }
+            });
+            thread.start();
+        }
+
+        @Override
+        public void onReceivePush(Client client, String content) {
+
+        }
+
+        @Override
+        public void onKickUser(String deviceId, String userId) {
+
+        }
+
     }
 }
